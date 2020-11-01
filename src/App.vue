2fe23cfd3dd0c1d23261a9605f555f4324651f99
Ekramul Islam Sumon <mmsumon799@gmail.com>
2020-11-01 11:12:54 +0600

    Transition Custom Class animation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="container">
-        <transition name="appear">
+        <transition
+                name="appear"
+                enter-active-class="animate__animated animate__bounce"
+                leave-active-class="animate__animated animate__swing"
+        >
             <div v-if="display" class="p-3 mb-3 bg-success text-white">Hello</div>
         </transition>
         <button @click="display=!display" class="btn btn-primary" type="submit">Toggle</button>
@@ -18,22 +22,5 @@
     }
 </script>
 <style>
-    .appear-enter {
-        opacity: 0;
-        transform: translateX(30px);
-    }
 
-    .appear-enter-active {
-        transition: all .3s ease;
-    }
-
-    .appear-leave {
-
-    }
-
-    .appear-leave-active {
-        opacity: 0;
-        transform: translateX(30px);
-        transition: all .3s ease;
-    }
 </style>
