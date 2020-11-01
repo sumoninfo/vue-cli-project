a88dc5c561b58673809f5e270cac1e5941e8f513
Ekramul Islam Sumon <mmsumon799@gmail.com>
2020-11-01 11:02:55 +0600

    Transition  animation

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,39 @@
 <template>
     <div class="container">
-        <div v-awesome.red.big="'Hello Sumon'"></div>
-        <div v-awesome.green.small="'Hello Sumon'"></div>
-        <hr>
-        <div v-user="userValue"></div>
+        <transition name="appear">
+            <div v-if="display" class="p-3 mb-3 bg-success text-white">Hello</div>
+        </transition>
+        <button @click="display=!display" class="btn btn-primary" type="submit">Toggle</button>
+
     </div>
 </template>
 
 <script>
-
     export default {
-        directives: {
-            'user': {
-                bind(el, binding) {
-                    el.innerHTML = binding.value
-                }
-            }
-        },
         data() {
             return {
-                userValue: 'Sakil'
+                display: false
             }
-        },
-
+        }
     }
 </script>
 <style>
-    body {
-        font-family: 'Roboto', sans-serif;
-        margin: 0;
+    .appear-enter {
+        opacity: 0;
+        transform: translateX(30px);
     }
 
+    .appear-enter-active {
+        transition: all .3s ease;
+    }
+
+    .appear-leave {
+
+    }
+
+    .appear-leave-active {
+        opacity: 0;
+        transform: translateX(30px);
+        transition: all .3s ease;
+    }
 </style>
